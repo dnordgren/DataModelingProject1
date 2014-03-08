f7289c51b64ca269f10524346ab3520385dab4c4
sjager <sawyer.jgr@gmail.com>
2014-03-08 10:40:09 -0800

    Update node.c and node.h to remove bugs. Add test_node.c as a blank compiling slate. Compiles except for undefined functions cmp() and new_node_path().

--- PartB_code/Data_models/node.c
+++ PartB_code/Data_models/node.c
@@ -1,29 +1,71 @@
 #include "node.h"
 
-node_t create_node(int fanout) {
-	node_t n;
-	n.fanout = fanout;
-	n.compare = malloc(sizeof(char)*(fanout-1));
+node_t* create_node(int fanout, char *node_path) {
+	node_t *n;
+	n->fanout = fanout;
+	n->compare = malloc(sizeof(char)*(fanout-1));
 	int i;
 	for (i = 0; i < fanout-1; i++) {
-		n.compare[i] = malloc(sizeof(char)*FILENAME_LENGTH);
+		n->compare[i] = malloc(sizeof(char)*FILENAME_LENGTH);
 	}
-	n.children = malloc(sizeof(node_t*)*fanout);
-	n.child_num = 0;
+	n->children = malloc(sizeof(char)*(fanout));
+	for(i = 0; i < fanout; i++) {
+		n->children[i] = malloc(sizeof(char)*FILENAME_LENGTH);
+	}
+	n->child_num = 0;
 	
+	write_node(n, node_path);
 	return n;
 }
 
-void free_node(node_t node) {
-	int i;
-	for (i = 0; i < fanout-1; i++) {
-		free(n.compare[i]);
-	}
-	free(n.compare);
-	free(n.children);
+node_t* read_node(char* node_path) {
+	FILE *infile = fopen(node_path, "rb");
+	node_t *n;
+
+	fread(&(n->fanout), sizeof(int), 1, infile);
+	fread((n->compare), sizeof(char**), 1, infile);
+	fread((n->children), sizeof(char**), 1, infile);
+	fread(&(n->child_num), sizeof(int), 1, infile);
+	fread((n->left_sibling), sizeof(char*), 1, infile);
+	fread((n->right_sibling), sizeof(char*), 1, infile);
+	fread(&(n->is_root), sizeof(bool), 1, infile);
+	fread(&(n->is_leaf), sizeof(bool), 1, infile);
+
+	fclose(infile);
+
+	return n;
 }
 
-int find_element(node_t *node, user_t *in_user, int min, int max) {
+void write_node(node_t *n, char* node_path) {
+	FILE *outfile = fopen(node_path, "wb");
+	
+	fwrite(&(n->fanout), sizeof(int), 1, outfile);
+	fwrite((n->compare), sizeof(char**), 1, outfile);
+	fwrite((n->children), sizeof(char**), 1, outfile);
+	fwrite(&(n->child_num), sizeof(int), 1, outfile);
+	fwrite((n->left_sibling), sizeof(char*), 1, outfile);
+	fwrite((n->right_sibling), sizeof(char*), 1, outfile);
+	fwrite(&(n->is_root), sizeof(bool), 1, outfile);
+	fwrite(&(n->is_leaf), sizeof(bool), 1, outfile);
+	
+	fclose(outfile);
+}
+
+void free_node(node_t *n) {
+	int i;
+	for (i = 0; i < n->fanout-1; i++) {
+		free(n->compare[i]);
+	}
+	for (i = 0; i < n->fanout; i++) {
+		free(n->children[i]);
+	}
+	free(n->children);
+	free(n->compare);
+}
+
+int find_element(char *node_path, user_t *in_user, int min, int max) {
+	node_t *node = read_node(node_path);
+
 	FILE *compare_file;
 	if (min < max) {
 		return min;
@@ -38,10 +80,10 @@ int find_element(node_t *node, user_t *in_user, int min, int max) {
 		fclose(compare_file);
 		
 		if (result == -1) {
-			return find_element(node, key, min, mid-1);
+			return find_element(node_path, user, min, mid-1);
 		}
 		else if (result == 1) {
-			return find_element(node, key, mid+1, max);
+			return find_element(node_path, user, mid+1, max);
 		}
 		// B+ tree defined to say matches go into right child
 		else {
@@ -50,14 +92,16 @@ int find_element(node_t *node, user_t *in_user, int min, int max) {
 	}
 }
 
-int insert_element(node_t *node, user_t *user, char *filepath) {
-	int find_result = find_element(user, 0, fanout-1);
+int insert_element(char *node_path, user_t *user, char *filepath) {
+	node_t *node = read_node(node_path);
+
+        int find_result = find_element(node_path, user, 0, node->fanout-1);
 	// if current node is a leaf
-	if (node->isLeaf) {
+	if (node->is_leaf) {
 		// leaf is full
 		if (node->fanout != node->child_num) {
 			int i;
-			for (i = child_num; i > find_result; i--) {
+			for (i = node->child_num; i > find_result; i--) {
 				node->compare[i] = node->compare[i-1];
 			}
 			node->compare[i] = filepath;
@@ -70,24 +114,26 @@ int insert_element(node_t *node, user_t *user, char *filepath) {
 	// not a leaf
 	else {
 		// if child is full
-		if (insert_element(user, filepath, node->children[find_result]) == -1) {
+		if (insert_element(filepath, user, node->children[find_result]) == -1) {
 			// cannot split because current node is full
 			if (node->fanout == node->child_num) {
 				return -1;
 			}
 			// split children
 			else {
-				split_page(node, find_result);
-				find_result = find_element(user, 0, fanout-1);
+				split_page(node_path, find_result);
+				find_result = find_element(node_path, user, 0, node->fanout-1);
 			}
 		}
 	}
 	// insert is a success
-	insert_element(user, filepath, node->children[find_result]);
+	insert_element(filepath, user, node->children[find_result]);
 	return 0;
 }
 
-int split_page(node_t *parent_node, int child_index) {
+int split_page(char *parent_node_path, int child_index) {
+	node_t *parent_node = read_node(parent_node_path);
+
 	int i, j, k, l;
 	// move children to the right to make space for new child
 	for (i = parent_node->child_num; i > child_index; i--) {
@@ -98,19 +144,25 @@ int split_page(node_t *parent_node, int child_index) {
 		parent_node->compare[j] = parent_node->compare[j-1];
 	}
 	// move middle child compare element into the parent compare
-	parent_node->compare[child_index] = parent_node->children[child_index]->compare[fanout/2];
+	node_t *child_node = read_node(parent_node->children[child_index]);
+	parent_node->compare[child_index] = child_node->compare[child_node->fanout/2];
 	// make new child
-	parent_node->children[child_index+1] = malloc(sizeof(node_t));
+	// TODO file names???
+	char* new_child_path = new_node_path();
+	node_t *new_child_node = create_node(parent_node->fanout, new_child_path);
+	parent_node->children[child_index+1] = new_child_path;
 	// move right half of previous child to new child 
-	for (k = fanout/2, l = 0; k < fanout; k++, l++) {
-		parent_node->children[child_index+1]->compare[l] = parent_node->children[child_index]->compare[k];
+	for (k = parent_node->fanout/2, l = 0; k < parent_node->fanout; k++, l++) {
+		new_child_node->compare[l] = child_node->compare[k];
 		// clear the previous child elements (compares) after moving
-		parent_node->children[child_index]->compare[k] = NULL;
+		child_node->compare[k] = NULL;
 	}
 	
 	// TODO handle if a split is needed, but can't split up because the root is full
 }
 
 //TODO int delete_element(char* node) {}
+
+
 
 // TODO write compare function cmp()
--- PartB_code/Data_models/node.h
+++ PartB_code/Data_models/node.h
@@ -9,31 +9,35 @@
 
 #define FILENAME_LENGTH 1024
 
-//TODO pass around filepaths, not nodes
-
 // node structure
 typedef struct {
-    int fanout;	// the number of pages that the node can hold
-    char **compare;	// pointer to an array of 'fanout-1' files
-	node_t *children;	// pointer to an array of 'fanout' nodes
+	int fanout;	// the number of pages that the node can hold
+	char **compare;	// pointer to an array of 'fanout-1' files
+	char **children;	// pointer to an array of 'fanout' nodes
 	int child_num;	// defines the number of pages that the node is holding
-	node_t *left_sibling;
-	node_t *right_sibling;
+	char *left_sibling;
+	char *right_sibling;
 	bool is_root;	// a node with no parent
 	bool is_leaf;	// a node with no children
 } node_t;
 
 // "constructor" for node structs
-node_t create_node(int fanout);
+node_t* create_node(int fanout, char *node_path);
 
 // frees the node
-void free_node(node_t node);
+void free_node(node_t *node);
+
+// read a node from a file
+node_t* read_node(char *node_path);
+
+// write a node to a file
+void write_node(node_t *n, char* node_path);
 
 // given an element, add to array if leaf, otherwise add to correct child
 // returns status of operation
 // returns -1 if child is full - will call split_page()
 // int insert_element(FILE *infile);
-int insert_element(node_t *node, user_t *user, char *filepath);
+int insert_element(char *node_path, user_t *user, char *filepath);
 
 // find an element, remove it from tree, redistribute if necessary
 // returns status of operation 
@@ -42,11 +46,11 @@ int delete_element(char *filepath);
 // compare infile to elements of compare[] (binary-like)
 // and return the index of the child it belongs to
 // return the index of the element
-int find_element(node_t *node, int key, int min, int max);
+int find_element(char *node_path, user_t *user, int min, int max);
 
 // splits the child index into two, copy the middle element to the current page
 // has to move up call chain recursively 
 // returns status of operation
-int split_page(node_t *parent_node, int child_index);
+int split_page(char *parent_node_path, int child_index);
 
 #endif
